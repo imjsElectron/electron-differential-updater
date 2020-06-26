ae5701620a9bcf8680939fd6178251ed5c195563
akshay-shrivastava <aksshayy@gmail.com>
2020-06-26 11:25:44 +0530

    supporting custom blockmap name

--- src/ElectronAppAdapter.ts
+++ src/ElectronAppAdapter.ts
@@ -1,43 +1,44 @@
-import * as path from "path"
-import { AppAdapter, getAppCacheDir } from "./AppAdapter"
+import * as path from "path";
+import { AppAdapter, getAppCacheDir } from "./AppAdapter";
 
 export class ElectronAppAdapter implements AppAdapter {
-  constructor(private readonly app = require("electron").app) {
-  }
+  constructor(readonly app = require("electron").app) {}
 
   whenReady(): Promise<void> {
-    return this.app.whenReady()
+    return this.app.whenReady();
   }
 
   get version(): string {
-    return this.app.getVersion()
+    return this.app.getVersion();
   }
 
   get name(): string {
-    return this.app.getName()
+    return this.app.getName();
   }
 
   get isPackaged(): boolean {
-    return this.app.isPackaged === true
+    return this.app.isPackaged === true;
   }
 
   get appUpdateConfigPath(): string {
-    return this.isPackaged ? path.join(process.resourcesPath!!, "app-update.yml") : path.join(this.app.getAppPath(), "dev-app-update.yml")
+    return this.isPackaged
+      ? path.join(process.resourcesPath!!, "app-update.yml")
+      : path.join(this.app.getAppPath(), "dev-app-update.yml");
   }
 
   get userDataPath(): string {
-    return this.app.getPath("userData")
+    return this.app.getPath("userData");
   }
 
   get baseCachePath(): string {
-    return getAppCacheDir()
+    return getAppCacheDir();
   }
 
   quit(): void {
-    this.app.quit()
+    this.app.quit();
   }
 
   onQuit(handler: (exitCode: number) => void): void {
-    this.app.once("quit", (_: Event, exitCode: number) => handler(exitCode))
+    this.app.once("quit", (_: Event, exitCode: number) => handler(exitCode));
   }
 }
